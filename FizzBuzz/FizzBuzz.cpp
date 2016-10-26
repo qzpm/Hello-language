8b186dc59ce6d7a1b9fa98875b148c11ac9659a2
qzpm <jliberecop@gmail.com>
2016-10-26 14:16:56 +0900

    Oops...

--- FizzBuzz/FizzBuzz.cpp
+++ FizzBuzz/FizzBuzz.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-  for (int i = 0; i <= 100; i++) {
+  for (int i = 1; i <= 100; i++) {
     if(i % 15 == 0) {
       std::cout << "Fizz Buzz" << std::endl;
     } else if(i % 3 == 0) {
